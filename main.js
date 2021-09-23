f178a2ac4a8c6d1d058027d678ad2769a90384be
Iversen1 <rasmus142@hotmail.com>
2021-09-23 18:53:54 +0200

    added buttons

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // Start with getting the X and O from the array into the HTML, 
+// find each square in the html
+// 
 // Module
 const gameBoard = (() => {
     let gameboard = ["O", "X", "0",
@@ -10,24 +12,32 @@ const gameBoard = (() => {
 const Player = (name, points) => {
     const getName = () => name;
     const getPoints = () => points;
-
-
 }
 
 const displayController = (() => {
-
 })
 
-
-function renderBoard (rows, cols) {
+function renderBoard () {
     let grid = document.getElementById("container");
-    for(i=0; i <rows * cols; i++) {
+    for(i=1; i<10; i++) {
         let singleGrid = document.createElement("div");
         grid.appendChild(singleGrid).className = "single-grid";
-        
-
-        
+        singleGrid.setAttribute("id", "grid" + i);
+        singleGrid.innerHTML = i;    
     }
+}
+renderBoard()
+
+var elements = document.getElementsByClassName("single-grid");
+
+for ( i= 0; i< elements.length; i++) {
+    elements[i].addEventListener("click", function() {
+        console.log("button"+ "clicked")
+    })
 
 }
-renderBoard(3,3)
+
+// Array.from(document.getElementsByClassName("single-grid")).forEach((element) => 
+//     addEventListener("click", function() {
+//         console.log(event.target)
+//     }) );
